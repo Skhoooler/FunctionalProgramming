ec41bbbea3ebde0dbcff4ff8fcdcfd6cc76f4613
David Doria <dor15004@byui.edu>
2023-02-15 22:28:53 -0700

    Week 7 challenge

---
+++ Week 7/challenge.erl
@@ -0,0 +1,9 @@
+-module(challenge).
+-export([lazy_main/0]).
+
+fibonaccify(Initial) -> fun(Next) -> Initial + (fibonaccify(Next))() end.
+
+lazy_main() ->
+    F1 = fibonaccify(1),
+    F2 = F1(2),
+    5 = (F2())().
